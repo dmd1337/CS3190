de561260b09f91864c544838674d182ec7a719ea
dmd1337 <dmd1337@hotmail.co.uk>
2015-11-03 17:47:45 +0000

    Removed AsymmetricCipher class

--- src/ciphers/AsymmetricCipher.java
+++
@@ -1,9 +0,0 @@
-package ciphers;
-/**
- * This class represents an asymmetric cipher. This has an additional key field to represent the private key; in addition, the encrypt and decrypt methods are not "mirror images" of each other.
- * @version 09/10/2015
- */
-public abstract class AsymmetricCipher extends Cipher
-{
-	protected String privateKey;
-}
